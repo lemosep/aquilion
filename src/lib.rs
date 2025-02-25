3e8338743ac50cab354c0e9694880e1e620a04f9
Eduardo Lemos <eduardolemosep@gmail.com>
2025-02-25 18:27:55 -0300

    [kernel]: Added VGA buffer support

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,22 @@
 #![no_std]
 #![no_main]
 
+//=================================================
+// Imports
+//=================================================
+
+#[macro_use]
+mod vga_buffer;
 use core::panic::PanicInfo;
+
+//=================================================
+// Standalone Functions
+//=================================================
 
 #[no_mangle]
 pub extern "C" fn kmain() {
-    let hello = b"Hello World!";
-    let color_byte = 0x1f; // white foreground, blue background
-
-    let mut hello_colored = [color_byte; 24];
-    for (i, char_byte) in hello.into_iter().enumerate() {
-        hello_colored[i*2] = *char_byte;
-    }
-
-    // write `Hello World!` to the center of the VGA text buffer
-    let buffer_ptr = (0xb8000 + 1988) as *mut _;
-    unsafe { *buffer_ptr = hello_colored };
-
+    vga_buffer::clear_screen();
+    println!("Hello, Aquilion!");
     loop{}
 }
 
